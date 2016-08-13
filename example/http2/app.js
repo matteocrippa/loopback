0d148f56f07320254e3f3338aa2ebb1ec9c11b40
Matteo <matteo@boostco.de>
2016-08-13 15:07:35 +0200

    improved with example http2

---
+++ example/http2/app.js
@@ -0,0 +1,35 @@
+// Copyright IBM Corp. 2013,2016. All Rights Reserved.
+// Node module: loopback
+// This file is licensed under the MIT License.
+// License text available at https://opensource.org/licenses/MIT
+
+var g = require('strong-globalize')();
+
+var loopback = require('../../');
+var app = loopback();
+
+app.set('protocol', 'http2');
+console.log(app.get('protocol'));
+
+app.set('sslCertPath', '../../test/fixtures/simple-app-http2/certs/server.crt');
+app.set('sslKeyPath', '../../test/fixtures/simple-app-http2/certs/server.key');
+
+app.use(loopback.rest());
+
+var dataSource = app.dataSource('db', { adapter: 'memory' });
+
+var Color = dataSource.define('color', {
+  'name': String,
+});
+
+Color.create({ name: 'red' });
+Color.create({ name: 'green' });
+Color.create({ name: 'blue' });
+
+Color.all(function() {
+  console.log(arguments);
+});
+
+app.listen(3000);
+
+g.log('test using: curl --http2 -I https://localhost:3000/colors --verbose --insecure');
